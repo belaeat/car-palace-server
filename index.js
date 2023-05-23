766aefc3e1680ba82c25d097dcef314a9aa37b22
belaeat <omyhossain@gmail.com>
2023-05-23 17:02:33 +0600

    toy added to the server

--- index.js
+++ index.js
@@ -26,12 +26,33 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // collections here
+        const categorizedToyCollection = client.db('carPalace').collection('categorizedToy')
+        const addedToyCollection = client.db('carPalace').collection('newAddedToy')
+
+        app.get('/categories', async (req, res) => {
+            const cursor = categorizedToyCollection.find()
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+
+        // adding a toy
+        app.post('/newAddedToy', async(req, res) => {
+            const newToy = req.body;
+            console.log(newToy)
+            const result = await addedToyCollection.insertOne(newToy)
+            res.send(result)
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
